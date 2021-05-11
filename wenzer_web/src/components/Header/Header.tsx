6a83241bc2a4fa5c79800cbe588d335213ce1905
matheus13f <matheus-goes13@hotmail.com>
2021-05-11 20:40:57 -0300

    inicializado

---
+++ wenzer_web/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export default function Header():ReactElement {
+    return (
+        <span>
+            Aqui esta o header
+        </span>
+    )
+}
