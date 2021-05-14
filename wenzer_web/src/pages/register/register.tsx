19390143574061bf5e9f2e504c1b3af10235ceb4
matheus13f <matheus-goes13@hotmail.com>
2021-05-13 23:14:31 -0300

    Rotas de login e registro configuradas

--- wenzer_web/src/pages/register/register.tsx
+++ wenzer_web/src/pages/register/register.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import Head from 'next/head';
 import { Paper } from '@material-ui/core';
-import { useStyles } from './styles';
+import React from 'react';
+import Layout from '../../components/Layout';
+import { useStyles } from '../styles';
 
-export default function Home() {
+export default function Register(){
   const classes = useStyles();
   return (
     <Layout>
-      <Head>
-        <title>Wenzer</title>
-      </Head>
       <Paper className={classes.root} elevation={20}>
         <form className={classes.formLogin}>
-          <h1>Log-in</h1>
+          <h1>Register</h1>
+          <input type="text" required placeholder="Nome" />
           <input type="email" required placeholder="E-mail" />
           <input type="password" required placeholder="Senha" />
-          <button type="submit" >Sign in</button>
+          <button type="submit">Sign in</button>
         </form>
       </Paper>
     </Layout>
   );
-}
+};
--- wenzer_web/src/pages/styles.tsx
+++ wenzer_web/src/pages/styles.tsx
@@ -2,13 +2,19 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    background: theme.palette.background.default,
+    background: theme.palette.secondary.main,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100vh',
-    width: '100vw',
+    height: '20vh',
+    width: '20vw',
     overflow: 'hidden',
   },
-}))
+  formLogin: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
