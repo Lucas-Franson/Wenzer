ce28ddc14caf56c9f90850a51562ce8d5c8db5c2
matheus13f <matheus-goes13@hotmail.com>
2021-05-14 20:42:19 -0300

    configurando conexao com back

--- wenzer_web/src/pages/register/register.tsx
+++ wenzer_web/src/pages/register/register.tsx
@@ -1,18 +1,57 @@
 import { Paper } from '@material-ui/core';
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import Layout from '../../components/Layout';
 import { useStyles } from '../styles';
+import { useRouter } from 'next/router';
 
 export default function Register(){
   const classes = useStyles();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  async function submit(e : SyntheticEvent) {
+    e.preventDefault();
+
+    await fetch('http://localhost:3333/api/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      })
+    })
+
+    await router.push('/login');
+  }
+
   return (
     <Layout>
       <Paper className={classes.root} elevation={20}>
-        <form className={classes.formLogin}>
+        <form className={classes.formLogin} onSubmit={submit}>
           <h1>Register</h1>
-          <input type="text" required placeholder="Nome" />
-          <input type="email" required placeholder="E-mail" />
-          <input type="password" required placeholder="Senha" />
+          <input
+            type="text"
+            required
+            placeholder="Nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            required
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button type="submit">Sign in</button>
         </form>
       </Paper>
