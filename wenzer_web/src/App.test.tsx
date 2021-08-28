d873f95b9f4c9ab3e8e9c7e97bef29216356facb
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-08-28 20:57:56 -0300

    Writing a example test web

---
+++ wenzer_web/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+describe("example test", () => {
+    it('two is equals to one plus one', () => {
+        expect(2).toEqual(1+1);
+    })
+})
