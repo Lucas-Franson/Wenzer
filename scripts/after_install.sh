104bbd9efc4b1e28fb7cadf1c1c10c2737898062
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-09-04 21:12:32 -0300

    Update env pm2 server js

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,6 @@ set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
 git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
-pm2 restart src/server.js --update-env
+pm2 restart /home/ec2-user/Wenzer_Project/wenzer_back/src/server.js --update-env
+pm2 save
 
