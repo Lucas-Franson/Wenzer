77fe9d914753ca52d3d77dd163987ae85a0de884
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-09-04 21:20:51 -0300

    Changed front end point to back end

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@ set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
 git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
-pm2 restart /home/ec2-user/Wenzer_Project/wenzer_back/src/server.js --update-env
+ENV_PROD=somethingnew pm2 restart server --update-env
 pm2 save
 
