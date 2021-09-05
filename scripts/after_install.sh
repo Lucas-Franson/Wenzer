4ca9209ec0156794565b5f0430a3ffd66dfc68cb
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-09-04 21:39:00 -0300

    Adicionando servico pm2

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@ set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
 git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
-pm2 restart $home/Wenzer_Project/wenzer_back/src/server.js --update-env
+pm2 start $home/Wenzer_Project/wenzer_back/src/server.js
 pm2 save
 
