f6c08317d6726284da7417a058385cd3487851cf
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-09-04 21:42:48 -0300

    set home path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@ set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
 git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
-pm2 start $home/Wenzer_Project/wenzer_back/src/server.js
+pm2 start /home/ec2-user/Wenzer_Project/wenzer_back/src/server.js
 pm2 save
 
