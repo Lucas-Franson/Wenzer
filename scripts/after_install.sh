e555365a69a0890d30b767de688924c0410fe6c4
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-09-04 21:31:09 -0300

    using a environment variable

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@ set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
 git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
-ENV_PROD=somethingnew pm2 restart server --update-env
+pm2 restart $home/Wenzer_Project/wenzer_back/src/server.js --update-env
 pm2 save
 
