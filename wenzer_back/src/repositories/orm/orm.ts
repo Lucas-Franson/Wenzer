540f3ee2d728604a717acdc3e01bcdf95f1f7a2b
Lucas-Franson <lucasfranson.8@hotmail.com>
2021-12-13 22:37:03 -0300

    Fix issue in entity name

--- wenzer_back/src/repositories/orm/orm.ts
+++ wenzer_back/src/repositories/orm/orm.ts
@@ -3,25 +3,25 @@ import { conexao, queryPromise } from '../conexao';
 
 export class Orm<T extends { id: string, validateObject: Function }> implements IOrm<T> {
     async get(whereClause: string): Promise<T> {
-        const tableName = this.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         const sql = `SELECT * FROM ${tableName} ${whereClause}`;
         let result: any = await queryPromise(sql);
         return result[0];
     }
     async getAll(whereClause: string): Promise<T[]> {
-        const tableName = this.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         const sql = `SELECT * FROM ${tableName} ${whereClause}`;
         let result: any = await queryPromise(sql);
         return result;
     }
     async getById(id: string): Promise<T> {
-        const tableName = this.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         const sql = `SELECT * FROM ${tableName} WHERE ID = '${id}' LIMIT 1`;
         let result: any = await queryPromise(sql);
         return result.length > 0 ? result[0] : null;
     }
     async insert(object: T): Promise<void> {
-        const tableName = this.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         if (!this.validateObject(object)) throw new Error(`${this._capitalizeFirstLetter(tableName)} possui dados incorretos.`);
         const sql = `INSERT INTO ${tableName} SET ?`;
         await conexao.query(sql, object, (err: any) => {
@@ -29,7 +29,7 @@ export class Orm<T extends { id: string, validateObject: Function }> implements 
         });
     }
     async update(object: T): Promise<void> {
-        const tableName = this.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         if (!this.validateObject(object)) throw new Error(`${this._capitalizeFirstLetter(tableName)} possui dados incorretos.`);
         const sql = `UPDATE ${tableName}
                      SET ${this._createSetOfData(object)}
@@ -61,7 +61,7 @@ export class Orm<T extends { id: string, validateObject: Function }> implements 
         }
     }
     async delete(object: T): Promise<void> {
-        const tableName = object.constructor["name"];
+        const tableName = this.constructor["name"].replace("Repository", "");
         const sql = `DELETE FROM ${tableName} WHERE ID = ${object.id}`;
         conexao.query(sql, (err: any) => {
             if (err) throw new Error(err);
