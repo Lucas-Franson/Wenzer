a82bea2955471109be8a293f1376ece6677385e8
Lucas Franson Barros <lucas.franson@ambevtech.com.br>
2022-03-04 21:30:06 -0300

    changed deploy continuous

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 cd /home/ec2-user/Wenzer_Project/wenzer_back
-git pull https://github.com/Lucas-Franson/Wenzer.git
 npm install
 pm2 start /home/ec2-user/Wenzer_Project/wenzer_back/dist/src/server.js -f
 pm2 save
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,6 @@
 set -e
 #yum update -y
 pm2 update
+cd /home/ec2-user/Wenzer_Project/wenzer_back
+git pull https://github.com/Lucas-Franson/Wenzer.git
+sudo rm -rf node_modules
