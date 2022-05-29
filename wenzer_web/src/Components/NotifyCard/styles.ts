b3c97c340c5be578a980c8bd4f37006483f6bb8c
matheus13f <matheus-goes13@hotmail.com>
2022-05-29 00:27:54 -0300

    Ajuste no css

--- wenzer_web/src/Components/NotifyCard/styles.ts
+++ wenzer_web/src/Components/NotifyCard/styles.ts
@@ -38,7 +38,7 @@ export const Container = styled.div`
         display: flex;
         align-items: center;
         justify-content: center;
-        gap: 10px;
+        gap: 11px;
       }
 
       > span {
